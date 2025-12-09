67cd8b94f61a5076a1441745569df5392da43a20
keremcadirci <kerem.cadirci@sistas.com.tr>
2025-12-09 10:16:14 +0300

    AlternativeService moved to 380,

--- src/core/SIP/SIPConstants.cs
+++ src/core/SIP/SIPConstants.cs
@@ -405,8 +405,8 @@ namespace SIPSorcery.SIP
         MultipleChoices = 300,
         MovedPermanently = 301,
         MovedTemporarily = 302,
-        AlternativeService = 304,
         UseProxy = 305,
+        AlternativeService = 380,
 
         // Client-Error
         BadRequest = 400,
